fad26a099b70744131e42bb5bfd13d67ce879ecd
0hyeon <djdjdjk2006@naver.com>
2021-06-30 16:16:00 +0900

    210630

--- src/main/index.js
+++ src/main/index.js
@@ -2,12 +2,20 @@ import React from "react";
 import "./index.css";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+
+import relativeTime from "dayjs/plugin/relativeTime"
+import {API_URL} from "../config/constants.js";
+import { Carousel } from 'antd';
+dayjs.extend(relativeTime);//dayjs에서 확장된 기능 사용 
 
 function MainPage() {
-  const [products, setProducts] = React.useState([]);
+  const [products, setProducts] = React.useState([]);// state형태
+  const [banners, setBanners] = React.useState([]);
   React.useEffect(function () {
     axios
-      .get("http://localhost:8080/products")
+      // 상품관련
+      .get(`${API_URL}/products`)
       .then(function (result) {
         const products = result.data.products;
         setProducts(products);
@@ -15,35 +23,60 @@ function MainPage() {
       .catch(function (error) {
         console.error("에러 발생 : ", error);
       });
+      // 배너관련
+      axios.get(`${API_URL}/banners`).then((result)=>{
+        const banners = result.data.banners;
+        setBanners(banners);
+      }).catch((error)=>{
+        console.error("에러발생",error);
+      })
   }, []);
 
   return (
     <div>
-      <div id="banner">
-        <img src="images/banners/banner1.png" />
-      </div>
-      <h1>판매되는 상품들</h1>
+    {/*배너*/}
+    <Carousel autoplay autoplaySpeed={3000}>
+      {
+        banners.map((banner,index) => {
+          return(
+            <Link to={banner.href}>
+              <div id="banner">
+                <img src= {`${API_URL}/${banner.imageUrl}`} alt="" />
+              </div>
+            </Link>
+          )
+        })
+      }
+    </Carousel>
+      <h1 id="product-headline">판매되는 상품들</h1>
       <div id="product-list">
+        {/* 상품리스트 */}
         {products.map(function (product, index) {
           return (
             <div className="product-card">
+              {
+                product.soldout === 1 && <div className="product-blur" />
+              }
               <Link
                 style={{ color: "inherit" }}
                 className="product-link"
                 to={`/products/${product.id}`}
               >
                 <div>
-                  <img className="product-img" src={product.imageUrl} />
+                  <img className="product-img" src={`${API_URL}/${product.imageUrl}`} alt="" />
                 </div>
                 <div className="product-contents">
                   <span className="product-name">{product.name}</span>
                   <span className="product-price">{product.price}원</span>
-                  <div className="product-seller">
-                    <img
-                      className="product-avatar"
-                      src="images/icons/avatar.png"
-                    />
-                    <span>{product.seller}</span>
+                  <div className="product-footer">
+                    <div className="product-seller">
+                      <img
+                        className="product-avatar"
+                        src="images/icons/avatar.png" alt=""
+                      />
+                      <span>{product.seller}</span>
+                    </div>
+                    <span className="product-date">{dayjs(product.createdAt).fromNow()}</span>
                   </div>
                 </div>
               </Link>
--- src/upload/index.js
+++ src/upload/index.js
@@ -1,10 +1,41 @@
-import { Form, Divider, Input, InputNumber, Button } from "antd";
+import { Form, Divider, Input, InputNumber, Button, Upload, message } from "antd";
 import "./index.css";
 import { ForkOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import {API_URL} from "../config/constants.js";
+import axios from 'axios';
+import { useHistory }from "react-router-dom";
+
 function UploadPage() {
-  const onSubmit = (values) => {
-    console.log(values);
+  const [imageUrl, setImageUrl] = useState(null);
+  const history = useHistory();//리액트훅
+
+  const onSubmit = (values) => {//제출
+    axios.post(`${API_URL}/products`,{
+      name : values.name,
+      description : values.description,
+      seller : values.seller,
+      price :  parseInt(values.price),
+      imageUrl : imageUrl,
+    }).then((result) =>{
+      console.log(result);//제출 잘됐으면 리디렉션
+      history.replace('/');//이전페이지의 기록이 사라지고 대체됨
+    }).catch((error) => {
+      console.log(error);
+      message.error(`에러가 발생했습니다. ${error.message}`)
+    });
+    ;
   };
+  const onChangeImage = (info) => {
+    if(info.file.status === 'uploading'){
+      return;
+    }
+    if(info.file.status === 'done'){
+      const response = info.file.response;
+      const imageUrl = response.imageUrl;
+      setImageUrl(imageUrl);
+    }
+  }
   return (
     <div id="upload-container">
       <Form name="상품 업로드" onFinish={onSubmit}>
@@ -12,10 +43,18 @@ function UploadPage() {
           name="upload"
           label={<div className="upload-label">상품 사진</div>}
         >
-          <div id="upload-img-placeholder">
-            <img src="/images/icons/camera.png" />
-            <span>이미지를 업로드해주세요.</span>
-          </div>
+          <Upload name="image" action={`${API_URL}/image`} listType="picture" showUploadList={false} onChange={onChangeImage}>
+            {
+              imageUrl ? (
+                <img id="upload-img" src= {`${API_URL}/${imageUrl}`} /> 
+              ) : (
+              <div id="upload-img-placeholder">
+                <img src="/images/icons/camera.png" />
+                <span>이미지를 업로드해주세요.</span>
+              </div>
+              )
+            }
+          </Upload>
         </Form.Item>
         <Divider />
         <Form.Item
