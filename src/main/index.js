7a436050f3fdbda21eb66fd3220c5b83e7dfda33
0hyeon <djdjdjk2006@naver.com>
2021-07-01 19:08:36 +0900

    210701

--- src/main/index.js
+++ src/main/index.js
@@ -7,8 +7,16 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime"
 import {API_URL} from "../config/constants.js";
 import { Carousel } from 'antd';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, Autoplay} from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
 dayjs.extend(relativeTime);//dayjs에서 확장된 기능 사용 
 
+// 모바일슬라이드
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay]);
 
 function MainPage() {
   const [products, setProducts] = React.useState([]);// state형태
@@ -38,8 +46,36 @@ function MainPage() {
       {/* <div id="top_banner" >
         <img src="images/ji/1.jpg"/>
       </div> */}
-    {/*배너*/}
-    <Carousel autoplay autoplaySpeed={2000}>
+    {/* 모바일배너 */}
+    
+    <Swiper
+      spaceBetween={0}
+      slidesPerView={1}
+      navigation
+      Autoplay
+      pagination={{ clickable: false }}
+      scrollbar={{ draggable: true }}
+      loop={true} 
+      autoplay={true}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      {
+        banners.map((banner,index) => {
+          return(
+            <SwiperSlide>
+               <Link to={banner.href}>
+                  <div id="banner">
+                    <img src= {`${API_URL}/${banner.imageUrl}`} alt="" />
+                  </div>
+                </Link>
+            </SwiperSlide>
+          )
+        })
+      }
+    </Swiper>
+  
+    {/*웹슬라이드배너*/}
+    {/* <Carousel autoplay autoplaySpeed={2000}>
       {
         banners.map((banner,index) => {
           return(
@@ -51,7 +87,7 @@ function MainPage() {
           )
         })
       }
-    </Carousel>
+    </Carousel> */}
       <h1 id="product-headline">NEW ARRIVALS</h1>
       <div id="product-list">
         {/* 상품리스트 */}
